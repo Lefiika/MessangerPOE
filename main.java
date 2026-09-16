929eaf700626573f4cb74ab4ab3caa1d45354074
Lefika Kolojane <lefikakolojane506@gmail.com>
2026-09-16 15:57:16 +0200

    Added the login section inside Main.java. It checks if registration passed before asking for login inputs and showing the login status message.

--- main.java
+++ main.java
@@ -23,6 +23,17 @@ public class Main {
         String regStatus = userAccount.registerUser();
         System.out.println(regStatus);
 
+        if (regStatus.contains("registered successfully")) {
+            System.out.print("\nEnter Username to Login: ");
+            String loginUser = scanner.nextLine();
+
+            System.out.print("Enter Password to Login: ");
+            String loginPass = scanner.nextLine();
+
+            boolean loginSuccess = userAccount.loginUser(loginUser, loginPass);
+            System.out.println(userAccount.returnLoginStatus(loginSuccess));
+        }
+
         scanner.close();
     }
 }
